3653b40e93c4d09694c1d9e55e4ae68510a93eaa
Ertem <ertem60@gmail.com>
2018-03-23 12:58:30 +0100

    mis en français

--- php/connexion.php
+++ php/connexion.php
@@ -55,7 +55,7 @@ else
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Bootstrap Admin Theme</title>
+    <title>Facturation Simple & Rapide</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
